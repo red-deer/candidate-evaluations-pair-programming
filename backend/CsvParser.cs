f0198d60b1f6aded5980e428178ab80881dfcd24
jezzgoodwin <jezzgoodwin@gmail.com>
2023-06-02 18:04:02 +0100

    backend test improvements

---
+++ backend/CsvParser.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace BackendTest;
+
+public class Sale
+{
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
+    public string City { get; set; } = "";
+    public string Item { get; set; } = "";
+    public int Quantity { get; set; } = 0;
+    public DateTime Date { get; set; } = DateTime.MinValue;
+    public decimal PricePerItem { get; set; } = 0;
+}
+
+public class CsvParser
+{
+    public List<Sale> ParseSales(string content)
+    {
+        var sales = new List<Sale>();
+        
+        var mapper = new Dictionary<string, Action<Sale, string>>
+        {
+            ["First Name"] = (sale, value) => sale.FirstName = value,
+            ["Last Name"] = (sale, value) => sale.LastName = value,
+            ["City"] = (sale, value) => sale.City = value,
+            ["Item"] = (sale, value) => sale.Item = value,
+            ["Quantity"] = (sale, value) => sale.Quantity = Convert.ToInt32(value),
+            ["Date"] = (sale, value) => sale.Date = DateTime.Parse(value, new CultureInfo("en-GB")),
+            ["Price Per Item"] = (sale, value) => sale.PricePerItem = Convert.ToDecimal(value)
+        };
+
+        var lines = content
+            .Replace("\r", "")
+            .Split("\n", StringSplitOptions.RemoveEmptyEntries);
+
+        var columnOrder = lines[0].Split(",");
+        
+        foreach (var line in lines.Skip(1))
+        {
+            var sale = new Sale();
+            
+            var cells = line.Split(",");
+            for (var i = 0; i < columnOrder.Length; i++)
+            {
+                mapper[columnOrder[i]](sale, cells[i]);
+            }
+            
+            sales.Add(sale);
+        }
+
+        return sales;
+    }
+}
---
+++ backend/README.md
@@ -0,0 +1,19 @@
+﻿## Code test
+
+Use any tools at your disposal, including nuget packages and online documentation. You can structure your code however you like.
+
+For the purposes of the exercise assume you do not need to handle any CSV features not present in the test files.
+
+### Tasks
+
+Read Sales CSV file and map to a collection of objects
+
+Find average cost of each item type sold
+
+Find top 3 months of sale revenue
+
+Compare 2 CSV files (Sales and SalesBackup) and find duplicate records
+
+Find average spend split by male and female
+
+Find most popular item sold per day of the week
